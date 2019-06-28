4fbac19539354dc524ea08015730794d95171f7c
afiorito <fioritoanthony@outlook.com>
2019-06-28 10:03:30 -0400

    Added ios single page application boilerplate + swiftlint

---
+++ Nightlight/AppDelegate.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+}
